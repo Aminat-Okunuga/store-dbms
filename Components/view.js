fb3079348663dafef19b0a165c4061f3628ba21a
Aminat-Okunuga <makadeaminat@gmail.com>
2021-04-17 23:42:44 +0100

    modified view

--- Components/view.js
+++ Components/view.js
@@ -1,42 +1,40 @@
-const express = require('express')
-const Router = express.Router()
-const model = require('./model')
+const express = require("express");
+const Router = express.Router();
+const model = require("./model");
 
 // add data
-exports.createEntry = async(req, res)=>{
-    const newData = await model.create(req.body);
-    res.status(201).json(newData);
-  };
+exports.createEntry = async (req, res) => {
+  const data = await model.create(req.body);
+  res.status(201).json(data);
+};
 
 //all data
-exports.getAllEntry = async(req, res)=>{
-    const data = await model.find();
-    res.status(200).json({
-        developer: "AminatCanCode",
-        report: true,
-        totalData: data.length + 1,
-        data: data
-    }) 
-}
+exports.getAllEntry = async (req, res) => {
+  const data = await model.find();
+  res.status(200).json({
+    developer: "AminatCanCode",
+    report: true,
+    totalData: data.length + 1,
+    data: data,
+  });
+};
 // find an item
-exports.getSingleEntry = async(req, res)=>{
-    const data = await model.findById(req.params.id);
-    res.status(200).json(data);
-}
+exports.getSingleEntry = async (req, res) => {
+  const data = await model.findById(req.params.id);
+  res.status(200).json(data);
+};
 
 // update an item
-exports.updateSingleEntry = async(req, res)=>{
-    const data = await model.findByIdAndUpdate(req.params.id, req.body);
-    res.status(200).json({
-        report: "Item sucessfully updated!",
-        data: data,
-    
-    });
-}
-  
-  // delete data
-  exports.deleteSingleEntry = async(req, res)=>{
-    const newData = await model.findByIdAndDelete(req.params.id, req.body);
-    res.status(200).json("Item Deleted Successfully!");
-  }
-    
+exports.updateSingleEntry = async (req, res) => {
+  const data = await model.findByIdAndUpdate(req.params.id, req.body);
+  res.status(200).json({
+    report: "Item sucessfully updated!",
+    data: data,
+  });
+};
+
+// delete data
+exports.deleteSingleEntry = async (req, res) => {
+  const data = await model.findByIdAndDelete(req.params.id, req.body);
+  res.status(200).json("Item successfully deleted!");
+};
